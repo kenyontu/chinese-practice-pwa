7fd81ffb31eb7c875c0e44f6954f71b38e8ce529
Kenyon Tu <kenyon.tu@itst.com.br>
2019-10-22 22:09:47 +0800

    fix: dialog animation

--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
+import { useSpring, animated } from 'react-spring'
 
 import styles from './Dialog.module.css'
 
@@ -9,12 +10,20 @@ interface Props {
 }
 
 const Dialog: React.FC<Props> = ({ children, isOpen, onBackdropClick }) => {
+  const dialogSpring = useSpring({
+    config: { duration: 200, delay: 100 },
+    opacity: isOpen ? 1 : 0,
+    transform: isOpen ? 'translateY(0)' : 'translateY(50%)',
+  })
+
   return (
     <div
       className={classNames(styles.backdrop, { [styles.open]: isOpen })}
       onClick={onBackdropClick}
     >
-      <div className={styles.dialog}>{children}</div>
+      <animated.div style={dialogSpring} className={styles.dialog}>
+        {isOpen ? children : null}
+      </animated.div>
     </div>
   )
 }
