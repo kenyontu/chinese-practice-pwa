1a5dd83f6b246c945a221fceadbf529b3fdea629
Kenyon Tu <kenyon.tu@itst.com.br>
2019-10-25 17:05:02 +0800

    feat: add chengyu

--- src/data/chengyu.ts
+++ src/data/chengyu.ts
@@ -5,10 +5,72 @@ const chengyu: WordList = {
   name: '成語',
   words: [
     {
+      id: '7wehLJ0S',
+      name: '青出於藍',
+      piyin: 'qīng chū yú lán',
+      description:
+        'the student/disciple becomes better than the teacher/master',
+    },
+    {
+      id: 'oLvy5Ueeq',
+      name: '大有可為',
+      piyin: 'dà yǒu kě wéi',
+      description: 'with great prospects for the future (idiom)',
+    },
+    {
+      id: 'r4FZQZOjx',
+      name: '後來居上',
+      piyin: 'hòu lái jū shàng',
+      description: 'the up-and-coming youngster outstrips the older generation',
+    },
+    {
+      id: 'HYTKy_7dc',
+      name: '同心協力',
+      piyin: 'tóng xīn xié lì',
+      description: 'to work with a common purpose',
+    },
+    {
       id: 'iV39ja_G',
       name: '刮目相看',
       piyin: 'guā mù xiāng kàn',
-      description: 'to have a new perspective than before',
+      description:
+        "to have a whole new level of respect for sb or sth, to sit up and take notice (of sb's improved performance etc)",
+    },
+    {
+      id: 'arotBl0t6',
+      name: '身居要職',
+      piyin: 'shēn jū yào zhí',
+      description: 'occupy an important position, hold an important post',
+    },
+    {
+      id: 'hW_ULNFoE',
+      name: '刮目相待',
+      piyin: 'guā mù xiāng dài',
+      description: 'regard with special esteem',
+    },
+    {
+      id: 'CoD1BqVfF',
+      name: '鶴立雞群',
+      piyin: 'hè lì jī qún',
+      description: 'stand out from the crowd',
+    },
+    {
+      id: 'nv-B1KLhB',
+      name: '出類拔萃',
+      piyin: 'chū lèi bá cuì',
+      description: 'surpassing, preeminent, outstanding',
+    },
+    {
+      id: 'O9ee--YyC',
+      name: '低人一等',
+      piyin: 'dī rén yī děng',
+      description: 'inferior to others',
+    },
+    {
+      id: '4w7rrgZ9s',
+      name: '雄偉挺拔',
+      piyin: 'xióng wěi tǐng bá',
+      description: 'majestic tall and straight',
     },
   ],
 }
